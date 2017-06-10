aee6c50edf3acc5d9a75d6b19bc01ca6648581dd
henriquessb <hssb8@hotmail.com>
2017-06-10 20:03:20 -0300

    Implementado parte da visualização, inserção e remoção de elementos da interface, além dos atributos das entidades

--- projeto_interface.cpp
+++ projeto_interface.cpp
@@ -13,51 +13,105 @@ int main ( int argc, char *argv[] ) {
   vector<Entidade> entidades;
   vector<string> atributosInseridos;
   string tmpString;
-  int opcao = -1, nroEntidade = -1;
+  int opcao = -1;
+  unsigned int nroEntidade, nroElemento;
 
   entidades.push_back( Entidade() );
   entidades.back().nome = "Condutor";
+  entidades.back().atributo.push_back( "ID" );
   entidades.back().atributo.push_back( "Nome" );
+  entidades.back().atributo.push_back( "RG" );
+  entidades.back().atributo.push_back( "Data de nascimento" );
+  entidades.back().atributo.push_back( "Telefone" );
+
+  entidades.push_back( Entidade() );
+  entidades.back().nome = "Habilitacao";
+  entidades.back().atributo.push_back( "Validade" );
+  entidades.back().atributo.push_back( "UF" );
+  entidades.back().atributo.push_back( "Orgao emissor" );
+  entidades.back().atributo.push_back( "Categoria" );
   entidades.back().atributo.push_back( "RG" );
 
   entidades.push_back( Entidade() );
-  entidades.back().nome = "Habilitação";
+  entidades.back().nome = "Veiculo";
+  entidades.back().atributo.push_back( "ID" );
+  entidades.back().atributo.push_back( "ID da empresa" );
+  entidades.back().atributo.push_back( "ID do condutor" );
+  entidades.back().atributo.push_back( "Placa" );
+  entidades.back().atributo.push_back( "Chassi" );
 
   entidades.push_back( Entidade() );
-  entidades.back().nome = "Veículo";
+  entidades.back().nome = "Marca de veiculo";
+  entidades.back().atributo.push_back( "ID" );
+  entidades.back().atributo.push_back( "Nome comercial" );
+  entidades.back().atributo.push_back( "Nome Fantasia" );
+  entidades.back().atributo.push_back( "Tipo" );
+  entidades.back().atributo.push_back( "Sede" );
 
   entidades.push_back( Entidade() );
-  entidades.back().nome = "Marca de veículo";
-
-  entidades.push_back( Entidade() );
-  entidades.back().nome = "Categoria de veículo";
+  entidades.back().nome = "Categoria de veiculo";
+  entidades.back().atributo.push_back( "ID" );
+  entidades.back().atributo.push_back( "Nome" );
+  entidades.back().atributo.push_back( "Quantidade de rodas" );
+  entidades.back().atributo.push_back( "Tracao" );
+  entidades.back().atributo.push_back( "Tipo" );
 
   entidades.push_back( Entidade() );
   entidades.back().nome = "Empresa de transporte";
+  entidades.back().atributo.push_back( "ID" );
+  entidades.back().atributo.push_back( "Nome comercial" );
+  entidades.back().atributo.push_back( "Nome fantasia" );
+  entidades.back().atributo.push_back( "Tipo" );
+  entidades.back().atributo.push_back( "Sede" );
 
   entidades.push_back( Entidade() );
   entidades.back().nome = "Local";
+  entidades.back().atributo.push_back( "ID" );
+  entidades.back().atributo.push_back( "Descricao" );
+  entidades.back().atributo.push_back( "CEP" );
+  entidades.back().atributo.push_back( "Tipo" );
+  entidades.back().atributo.push_back( "UF" );
 
   entidades.push_back( Entidade() );
-  entidades.back().nome = "Infração";
+  entidades.back().nome = "Infracao";
+  entidades.back().atributo.push_back( "ID" );
+  entidades.back().atributo.push_back( "Situacao" );
+  entidades.back().atributo.push_back( "Data" );
+  entidades.back().atributo.push_back( "Vencimento" );
+  entidades.back().atributo.push_back( "Velocidade aferida" );
 
   entidades.push_back( Entidade() );
-  entidades.back().nome = "Tipo de infração";
+  entidades.back().nome = "Tipo de infracao";
+  entidades.back().atributo.push_back( "ID" );
+  entidades.back().atributo.push_back( "Descricao" );
+  entidades.back().atributo.push_back( "Classificacao" );
+  entidades.back().atributo.push_back( "Pontuacao" );
+  entidades.back().atributo.push_back( "Valor" );
 
   entidades.push_back( Entidade() );
   entidades.back().nome = "Via";
+  entidades.back().atributo.push_back( "ID" );
+  entidades.back().atributo.push_back( "Identificacao" );
+  entidades.back().atributo.push_back( "Classificacao" );
+  entidades.back().atributo.push_back( "Velocidade" );
+  entidades.back().atributo.push_back( "UF" );
 
   entidades.push_back( Entidade() );
-  entidades.back().nome = "Sinalização";
+  entidades.back().nome = "Sinalizacao";
+  entidades.back().atributo.push_back( "ID" );
+  entidades.back().atributo.push_back( "Descricao" );
+  entidades.back().atributo.push_back( "Tipo" );
+  entidades.back().atributo.push_back( "Atributo 2" );
+  entidades.back().atributo.push_back( "Atributo 3" );
 
   entidades.push_back( Entidade() );
-  entidades.back().nome = "Fiscalização";
+  entidades.back().nome = "Fiscalizacao";
 
   entidades.push_back( Entidade() );
-  entidades.back().nome = "Órgão de trânsito";
+  entidades.back().nome = "Orgao de transito";
 
   entidades.push_back( Entidade() );
-  entidades.back().nome = "Funcionário do órgão";
+  entidades.back().nome = "Funcionario do orgao";
 
   cout << "Bem vindo ao Banco de Dados de Transporte do Governo Brasileiro" << endl;
 
@@ -76,40 +130,107 @@ int main ( int argc, char *argv[] ) {
     case 0:
       break;
     case 1:
-      cout << "Escolha uma entidade: " << endl;
+      cout << "Escolha uma entidade para inserir: " << endl;
 
       for ( unsigned int i = 0; i < entidades.size(); i++ )
         cout << i + 1 << " - " << entidades[i].nome << endl;
 
       cout << "Entidade: ";
       cin >> nroEntidade;
-      cout << endl << "Escolhida a entidade: '" << entidades[nroEntidade - 1].nome << "'" << endl;
+      if ( nroEntidade > 0 && nroEntidade <= entidades.size() ) {
+        cout << endl << "Escolhida a entidade: '" << entidades[nroEntidade - 1].nome << "'" << endl;
 
-      for ( unsigned int i = 0; i < entidades[nroEntidade - 1].atributo.size(); i++ ) {
-        cout << "Insira um dado para o atributo '" << entidades[nroEntidade - 1].atributo[i] << "': ";
-        cin >> tmpString;
-        atributosInseridos.push_back( tmpString );
-        tmpString.clear();
+        for ( unsigned int i = 0; i < entidades[nroEntidade - 1].atributo.size(); i++ ) {
+          cout << "Insira um dado para o atributo '" << entidades[nroEntidade - 1].atributo[i] << "': ";
+          cin >> tmpString;
+          atributosInseridos.push_back( tmpString );
+          tmpString.clear();
+        }
+
+        // Inserir SQL de inserção aqui
+
+        for ( unsigned int i = 0; i < entidades[nroEntidade - 1].atributo.size(); i++ )
+          atributosInseridos[i].clear();
+        atributosInseridos.clear();
       }
-
-      // Inserir SQL de inserção aqui
-
-      for ( unsigned int i = 0; i < entidades[nroEntidade - 1].atributo.size(); i++ )
-        atributosInseridos[i].clear();
-      atributosInseridos.clear();
+      else
+        cout << endl << "Entidade invalida! " << endl;
 
       break;
+
     case 2:
+      cout << "Escolha uma entidade para visualizar: " << endl;
+
+      for ( unsigned int i = 0; i < entidades.size(); i++ )
+        cout << i + 1 << " - " << entidades[i].nome << endl;
+
+      cout << "Entidade: ";
+      cin >> nroEntidade;
+
+      if ( nroEntidade > 0 && nroEntidade <= entidades.size() ) {
+        cout << endl << "Escolhida a entidade: '" << entidades[nroEntidade - 1].nome << "'" << endl;
+
+        // Inserir SQL de visualização aqui
+      }
+      else
+        cout << endl << "Entidade invalida! " << endl;
 
       break;
+
     case 3:
+      cout << "Escolha uma entidade para modificar: " << endl;
+
+      for ( unsigned int i = 0; i < entidades.size(); i++ )
+        cout << i + 1 << " - " << entidades[i].nome << endl;
+
+      cout << "Entidade: ";
+      cin >> nroEntidade;
+
+      if ( nroEntidade > 0 && nroEntidade <= entidades.size() ) {
+        cout << endl << "Escolhida a entidade: '" << entidades[nroEntidade - 1].nome << "'" << endl;
+        cout << "Escolha o elemento da entidade: ";
+        cin >> nroElemento;
+
+        cout << endl << "Para cada atributo do elemento '" << nroElemento << "', insira um dado para cada atributo" << endl;
+        for ( unsigned int i = 0; i < entidades[nroEntidade - 1].atributo.size(); i++ ) {
+          cout << "Insira um dado para o atributo '" << entidades[nroEntidade - 1].atributo[i] << "': ";
+          cin >> tmpString;
+          atributosInseridos.push_back( tmpString );
+          tmpString.clear();
+        }
+
+        // Inserir SQL de modificação aqui
+
+        for ( unsigned int i = 0; i < entidades[nroEntidade - 1].atributo.size(); i++ )
+          atributosInseridos[i].clear();
+        atributosInseridos.clear();
+      }
+      else
+        cout << endl << "Entidade invalida! " << endl;
 
       break;
+
     case 4:
+      cout << "Escolha uma entidade para remover: " << endl;
 
+      if ( nroEntidade > 0 && nroEntidade <= entidades.size() ) {
+        for ( unsigned int i = 0; i < entidades.size(); i++ )
+          cout << i + 1 << " - " << entidades[i].nome << endl;
+
+        cout << "Entidade: ";
+        cin >> nroEntidade;
+        cout << endl << "Escolhida a entidade: '" << entidades[nroEntidade - 1].nome << "'" << endl;
+        cout << "Escolha o elemento da entidade: ";
+        cin >> nroElemento;
+
+        // Inserir SQL de remoção aqui
+      }
+      else
+        cout << endl << "Entidade invalida! " << endl;
       break;
+
     default:
-      cout << "Opção inválida, por favor escolha outra opção" << endl;
+      cout << "Opcao invalida! Por favor escolha outra opcao" << endl;
       break;
     }
   }
