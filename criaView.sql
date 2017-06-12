3bf05603d915ba2e333987d32dc17a318ed8bcc4
gerudoking <edsantos2003@gmail.com>
2017-06-11 21:43:33 -0300

    Mudança na criação da view

--- criaView.sql
+++ criaView.sql
@@ -1,12 +1,2 @@
-CREATE 
-    ALGORITHM = UNDEFINED 
-    DEFINER = `root`@`localhost` 
-    SQL SECURITY DEFINER
-VIEW `viasdf` AS
-    SELECT 
-        `via`.`id` AS `id`,
-        `via`.`Identificação` AS `Identificação`
-    FROM
-        `via`
-    WHERE
-        (`via`.`UF` = 'DF')
+CREATE VIEW viasdf AS
+SELECT id, Identificação FROM via WHERE UF='DF'
