4d0a22f5ad8c20fe7610c992752fded4c1cc0a1d
gerudoking <edsantos2003@gmail.com>
2017-06-13 22:43:33 -0300

    Trigger mudada

--- criaTrigger.sql
+++ criaTrigger.sql
@@ -1,10 +1,10 @@
 DELIMITER $$
 
-CREATE TRIGGER cobrmotr AFTER INSERT
-ON cobrador
+CREATE TRIGGER insere_cobrador_apos_empregado AFTER INSERT
+ON empregado_emp_trans
 FOR EACH ROW
 BEGIN
-	INSERT INTO empregado_emp_trans VALUES (id, 'indefinido', CPF, RG, `Órgão expedidor`);
+	INSERT INTO cobrador (`id`) VALUES (new.id);
 END$$
 
 DELIMITER ;
